e7f78ced0c2a32ef9b10eb573f9dbf2f81df402c
amamat20 <97866041+amamat20@users.noreply.github.com>
2022-05-11 20:04:20 +0700

    Add files via upload

---
+++ dashboard/assets/js/pages/dashboard.js
@@ -0,0 +1,117 @@
+document.documentElement.style.setProperty('--linknya', '#0dcaf0');
+
+
+
+var optionsProfileVisit = {
+	annotations: {
+		position: 'back'
+	},
+	dataLabels: {
+		enabled:false
+	},
+	chart: {
+		type: 'bar',
+		height: 300
+	},
+	fill: {
+		opacity:1
+	},
+	plotOptions: {
+	},
+	series: [{
+		name: 'siswa',
+		data: [4720,4703,4689,1504,0]
+	}],
+	colors: '#0dcaf0',
+	xaxis: {
+		categories: ["Jan","Feb","Mar","Apr","Mei"],
+	},
+}
+let optionsVisitorsProfile  = {
+	series: [3481, 1272],
+	labels: ['Sudah mengumpulkan', 'Belum mengumpulkan'],
+	colors: ['#00D100','#F22C01'],
+	chart: {
+		type: 'pie',
+		width: '100%',
+		height:'350px'
+	},
+	legend: {
+		position: 'bottom'
+	},
+	plotOptions: {
+		donut: {
+			pie: {
+				size: '30%'
+			}
+		}
+	}
+}
+
+var optionsEurope = {
+	series: [{
+		name: 'series1',
+		data: [310, 800, 600, 430, 540, 340, 605, 805,430, 540, 340, 605]
+	}],
+	chart: {
+		height: 80,
+		type: 'area',
+	},
+	colors: ['#5350e9'],
+	stroke: {
+		width: 2,
+	},
+	grid: {
+		show:false,
+	},
+	dataLabels: {
+		enabled: false
+	},
+	xaxis: {
+		type: 'datetime',
+		categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z","2018-09-19T07:30:00.000Z","2018-09-19T08:30:00.000Z","2018-09-19T09:30:00.000Z","2018-09-19T10:30:00.000Z","2018-09-19T11:30:00.000Z"],
+		axisBorder: {
+			show:false
+		},
+		axisTicks: {
+			show:false
+		},
+		labels: {
+			show:false,
+		}
+	},
+	show:false,
+	yaxis: {
+		labels: {
+			show:false,
+		},
+	},
+	tooltip: {
+		x: {
+			format: 'dd/MM/yy HH:mm'
+		},
+	},
+};
+
+let optionsAmerica = {
+	...optionsEurope,
+	colors: ['#008b75'],
+}
+let optionsIndonesia = {
+	...optionsEurope,
+	colors: ['#dc3545'],
+}
+
+
+
+var chartProfileVisit = new ApexCharts(document.querySelector("#chart-profile-visit"), optionsProfileVisit);
+var chartVisitorsProfile = new ApexCharts(document.getElementById('chart-visitors-profile'), optionsVisitorsProfile)
+var chartEurope = new ApexCharts(document.querySelector("#chart-europe"), optionsEurope);
+var chartAmerica = new ApexCharts(document.querySelector("#chart-america"), optionsAmerica);
+var chartIndonesia = new ApexCharts(document.querySelector("#chart-indonesia"), optionsIndonesia);
+
+chartIndonesia.render();
+chartAmerica.render();
+chartEurope.render();
+chartProfileVisit.render();
+chartVisitorsProfile.render()
